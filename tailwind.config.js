687d81bcecdea1df1ec63e90cac790aaddb81f9d
Mateus Pereira <mateus.pereira4@gmail.com>
2024-10-03 07:42:47 -0300

    página de tarefas, estilização, funcionalidades. primeira versão do app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Adiciona o tailwind nos arquivos do diretório /src com extesão .js .ts .jsx .tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
